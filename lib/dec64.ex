eb25a6ee4bc8b5794cc618bf58e2d5a0980f3c95
Javier Gonel <bolibic@gmail.com>
2017-05-23 15:42:41 +0300

    More new test cases and improvements

--- lib/dec64.ex
+++ lib/dec64.ex
@@ -1,24 +1,43 @@
 defmodule Dec64 do
   @type t :: <<_::64>>
 
+  @min_c -36028797018963968
+  @max_c 36028797018963967
+
   defmacrop int_b(num, min, max) do
     quote do: unquote(num) >= unquote(min) and unquote(num) <= unquote(max)
   end
 
   def new(0, _), do: zero()
   def new(coefficient, exponent)
-    when int_b(coefficient, -36028797018963968, 36028797018963967)
-      and int_b(exponent, -127, 128),
+    when int_b(coefficient, @min_c, @max_c)
+      and int_b(exponent, -127, 127),
     do: <<coefficient::56>> <> <<exponent::8>>
 
+  def new(coefficient, exponent)
+    when exponent >= 127 and coefficient < @min_c,
+    do: nan()
+
+  def new(coefficient, exponent)
+    when exponent > 127,
+    do: new(coefficient * 10, exponent - 1)
+
+  def new(coefficient, exponent)
+    when not int_b(coefficient, @min_c, @max_c) or exponent < -127
+  do
+    extra = if (abs(rem(coefficient, 10)) > 4), do: 1, else: 0
+    extra = if (coefficient >= 0), do: extra, else: -extra
+    new(div(coefficient, 10) + extra, exponent + 1)
+  end
 
   def nan, do: <<0x80::64>>
   def zero, do: <<0::64>>
   def one, do: <<0x100::64>>
   def negative_one, do: <<0xFFFFFFFFFFFFFF00::64>>
 
-  @spec sum(Dec64.t, Dec64.t) :: Dec64.t
+  @spec sum(t, t) :: t
   def sum(_, _), do: nan()
+
 end
 
 defmodule Dec64.Operators do
